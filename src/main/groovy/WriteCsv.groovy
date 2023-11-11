95e143a78bf93a8fbfccd9966ebe68e2951e6479
Lakshmi <varu.lak@gmail.com>
2023-11-11 07:17:18 -0500

    write to csv

---
+++ src/main/groovy/WriteCsv.groovy
@@ -0,0 +1,10 @@
+def csvContent = "Name, Age, City\nJohn, 25, New York\nAlice, 30, London"
+
+def filePath = "C:\\Users\\Lakshmi\\IdeaProjects\\GroovyTest\\src\\main\\resources\\output\\example.csv"
+
+// Write to CSV file
+new File(filePath).withWriter { writer ->
+    writer.write(csvContent)
+}
+
+// Close the file
