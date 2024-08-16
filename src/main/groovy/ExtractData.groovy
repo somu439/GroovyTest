289a0f9b92583c5d7ec54f97392d2bc38e6b0530
Lakshmi <varu.lak@gmail.com>
2024-08-16 10:58:26 -0400

    macros to pop the data

---
+++ src/main/groovy/ExtractData.groovy
@@ -0,0 +1,28 @@
+import groovy.json.JsonSlurper
+
+def inputFile = new File('D:\\Sreeni\\Discover\\test.json')
+def outputFile = new File('D:\\Sreeni\\Discover\\test.csv')
+
+def jsonSlurper = new JsonSlurper()
+def data = jsonSlurper.parse(inputFile)
+
+def nameMap = [:]
+
+// Extract the relevant information
+data.each { entry ->
+    def name = entry.name
+    def id = entry.id
+    if (!nameMap.containsKey(name) || nameMap[name].id < id) {
+        nameMap[name] = [id: id, address: entry.address, phone: entry.phone]
+    }
+}
+
+// Write to CSV
+outputFile.withWriter { writer ->
+    writer.writeLine('ID,Name,Address,Phone')
+    nameMap.each { name, info ->
+        writer.writeLine("${info.id},${name},${info.address},${info.phone}")
+    }
+}
+
+println 'CSV file created successfully.'
